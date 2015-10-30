7ddec58d2c29a8c0ba3cb6a683f7bc91a63ee4a0
Stian Haga <hagastian@gmail.com>
2015-10-31 00:38:27 +0100

    Respond with text, not JSON

--- magicbot.js
+++ magicbot.js
@@ -1,13 +1,11 @@
 module.exports = function(req, res, next){
   console.log(req.body);
   var userName = req.body.user_name;
-  var botPayload = {
-    text : 'Hello, ' + userName + '!'
-  };
+  var text = 'Hello, ' + userName + '!';
 
   // avoid infinite loop
   if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
+    return res.status(200).send(text);
   } else {
     return res.status(200).end();
   }
